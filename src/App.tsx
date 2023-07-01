ccc8f105fac1d46de5eb84314ac04a7443fa34d4
ElsayedRapaa <Elsayedrapaa7@gmail.com>
2023-07-01 08:24:12 +0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1
+        className="
+            text-2xl
+            font-bold
+          "
+      >
+        Welcome In Store
+      </h1>
+    </div>
+  );
+}
+
+export default App;
